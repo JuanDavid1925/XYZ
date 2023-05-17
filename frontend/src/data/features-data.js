7adb7341e1d195fcd3009e424b382f02249aad52
JuanDavid1925 <juan.cabrera.lopez@correounivalle.edu.co>
2023-05-16 19:59:16 -0500

    Landing page

---
+++ frontend/src/data/features-data.js
@@ -0,0 +1,31 @@
+import {
+  StarIcon,
+  ArrowPathIcon,
+  FingerPrintIcon,
+} from "@heroicons/react/24/solid";
+
+export const featuresData = [
+  {
+    color: "blue",
+    title: "Búsqueda de hoteles",
+    icon: StarIcon,
+    description:
+      "Contamos con una potente función de búsqueda que te permite encontrar hoteles en cualquier destino que desees. Puedes filtrar los resultados por ubicación, precio, categoría de hotel, servicios ofrecidos y puntuaciones de los huéspedes. ",
+  },
+  {
+    color: "red",
+    title: "Planificación de itinerarios",
+    icon: DocumentCheckIcon,
+    description:
+      "Te ayudamos a crear itinerarios detallados para tus viajes. Puedes agregar actividades, atracciones turísticas, restaurantes y otros puntos de interés en tu planificación diaria.",
+  },
+  {
+    color: "teal",
+    title: "Verified Company",
+    icon: FingerPrintIcon,
+    description:
+      "Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!",
+  },
+];
+
+export default featuresData;
---
+++ frontend/src/routes.jsx
@@ -0,0 +1,37 @@
+import { Landing_page, SignIn, SignUp } from "./pages";
+import {
+  HomeIcon,
+  ArrowRightOnRectangleIcon,
+  UserPlusIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/24/solid";
+
+export const routes = [
+  {
+    icon: HomeIcon,
+    name: "home",
+    path: "/home",
+    element: <Landing_page />,
+  },
+  {
+    icon: ArrowRightOnRectangleIcon,
+    name: "Sign In",
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    icon: UserPlusIcon,
+    name: "Sign Up",
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    icon: DocumentTextIcon,
+    name: "Docs",
+    href: "https://www.material-tailwind.com/docs/react/installation",
+    target: "_blank",
+    element: "",
+  },
+];
+
+export default routes;
