53755f8732f528fa820d27fc02e4ebbf0632cae2
AlivUV <alvaro.portocarrero@correounivalle.edu.co>
2023-06-29 21:40:35 -0500

    Añadir urls del frontend.

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,3 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .views import UserView, RegisterView, LoginView, LogoutView
 from rest_framework.documentation import include_docs_urls
 from django.urls import path
@@ -8,8 +7,5 @@ urlpatterns = [
     path('XYZ-api/v1/register/', RegisterView.as_view()),
     path('XYZ-api/v1/login/', LoginView.as_view()),
     path('XYZ-api/v1/logout/', LogoutView.as_view()),
-    path('XYZ-api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('XYZ-api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('XYZ-api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users-crud-docs/', include_docs_urls(title='Users API'))
 ]
