e7ca6fb5ee3c2a9ff43b56ca20246395e73155a5
Karen Moreno <53530934+karenmoreno17@users.noreply.github.com>
2023-06-30 07:05:39 -0500

    Update dashboardUser.js

--- frontend/src/pages/dashboardUser.js
+++ frontend/src/pages/dashboardUser.js
@@ -15,11 +15,11 @@ export default function DashboardUser() {
   return (
     <div className="mt-10 ml-20">
       <Typography variant="h3" sx={{ mb: 5 }} >
-        Hola, Bienvenido
+        Hola, bienvenid@
       </Typography>
       <div className="w-72 mt-20 ">
         <Select
-          label="Selecciona una ciudad"
+          label="Cali"
           animate={{
             mount: { y: 0 },
             unmount: { y: 25 },
@@ -42,40 +42,39 @@ export default function DashboardUser() {
 
       <Grid container spacing={5}>
         <Grid item xs={12} sm={6} md={3}>
-          <Card className="w-96">
+          <Card className="w-66">
             <CardHeader shadow={false} floated={false} className="h-96">
               <img
-                src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+                src="https://cf.bstatic.com/xdata/images/hotel/square200/64120960.jpg?k=45bb7ab0f44f4b55567f20b7595a33cb2f7aec590bbe8bb3abb290a3cc4d6b44&o="
                 className="w-full h-full object-cover"
               />
             </CardHeader>
             <CardBody>
               <div className="flex items-center justify-between mb-2">
                 <Typography color="blue-gray" className="font-medium">
-                  Apple AirPods
+                  Hotel Dann Cali
                 </Typography>
                 <Typography color="blue-gray" className="font-medium">
-                  $95.00
-                </Typography>
-                <Typography color="blue-gray" className="font-medium">
-                  $95.00
-                </Typography>
-                <Typography color="blue-gray" className="font-medium">
-                  $95.00
+                  $304.000
                 </Typography>
               </div>
               <Typography variant="small" color="gray" className="font-normal opacity-75">
-                With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.
+                Habitación Doble
               </Typography>
             </CardBody>
             <CardFooter className="pt-0">
-              <Button
-                ripple={false}
-                fullWidth={true}
-                className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-              >
-                Add to Cart
-              </Button>
+              <a href="https://www.hotelesdanncali.com/">
+                <Button
+                  ripple={false}
+                  fullWidth={true}
+                  className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
+
+                >
+                  Página principal
+                </Button>
+              </a>
+
+
             </CardFooter>
           </Card>
         </Grid>
@@ -84,34 +83,36 @@ export default function DashboardUser() {
 
 
         <Grid item xs={12} sm={6} md={3}>
-          <Card className="w-96">
+          <Card className="w-66">
             <CardHeader shadow={false} floated={false} className="h-96">
               <img
-                src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+                src="https://cf.bstatic.com/xdata/images/hotel/square200/441812075.jpg?k=31a2a88656335970658e5e96cffd1f94baa5368baa245236640c7c91ac4d0eee&o="
                 className="w-full h-full object-cover"
               />
             </CardHeader>
             <CardBody>
               <div className="flex items-center justify-between mb-2">
                 <Typography color="blue-gray" className="font-medium">
-                  Apple AirPods
+                  Hotel Spiwak Spirito
                 </Typography>
                 <Typography color="blue-gray" className="font-medium">
-                  $95.00
+                  $285.000
                 </Typography>
               </div>
               <Typography variant="small" color="gray" className="font-normal opacity-75">
-                With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.
-              </Typography>
+                Habitación Doble - 2 camas dobles              </Typography>
             </CardBody>
             <CardFooter className="pt-0">
-              <Button
-                ripple={false}
-                fullWidth={true}
-                className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-              >
-                Add to Cart
-              </Button>
+              <a href="https://spiwak.com/">
+                <Button
+                  ripple={false}
+                  fullWidth={true}
+                  className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
+
+                >
+                  Página principal
+                </Button>
+              </a>
             </CardFooter>
           </Card>
         </Grid>
@@ -119,135 +120,108 @@ export default function DashboardUser() {
 
 
         <Grid item xs={12} sm={6} md={3}>
-          <Card className="w-96">
+          <Card className="w-66">
             <CardHeader shadow={false} floated={false} className="h-96">
               <img
-                src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+                src="https://cf.bstatic.com/xdata/images/hotel/square200/4148718.jpg?k=fdeca134a326c9632feea9fe509399d6bd2e2967428e7299a29b494f4b7f0273&o="
                 className="w-full h-full object-cover"
               />
             </CardHeader>
             <CardBody>
               <div className="flex items-center justify-between mb-2">
                 <Typography color="blue-gray" className="font-medium">
-                  Apple AirPods
+                  Now Hotel
                 </Typography>
                 <Typography color="blue-gray" className="font-medium">
-                  $95.00
+                  $260.000
                 </Typography>
               </div>
               <Typography variant="small" color="gray" className="font-normal opacity-75">
-                With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.
-              </Typography>
+                Suite Estándar              </Typography>
             </CardBody>
             <CardFooter className="pt-0">
-              <Button
-                ripple={false}
-                fullWidth={true}
-                className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-              >
-                Add to Cart
-              </Button>
+              <a href="https://nowhotel.com.co/">
+                <Button
+                  ripple={false}
+                  fullWidth={true}
+                  className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
+
+                >
+                  Página principal
+                </Button>
+              </a>
             </CardFooter>
           </Card>
         </Grid>
 
 
         <Grid item xs={12} sm={6} md={3}>
-          <Card className="w-96">
+          <Card className="w-66">
             <CardHeader shadow={false} floated={false} className="h-96">
               <img
-                src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+                src="https://cf.bstatic.com/xdata/images/hotel/square200/410434169.jpg?k=85685026ce3a09b198fbd1a347126270422a0331a8d62780180a05e9f89386cb&o="
                 className="w-full h-full object-cover"
               />
             </CardHeader>
             <CardBody>
               <div className="flex items-center justify-between mb-2">
                 <Typography color="blue-gray" className="font-medium">
-                  Apple AirPods
+                  Hotel Intercontinental Cali, an IHG Hotel
                 </Typography>
                 <Typography color="blue-gray" className="font-medium">
-                  $95.00
+                  $423.920
                 </Typography>
               </div>
               <Typography variant="small" color="gray" className="font-normal opacity-75">
-                With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.
-              </Typography>
+                Habitación Estándar              </Typography>
             </CardBody>
             <CardFooter className="pt-0">
-              <Button
-                ripple={false}
-                fullWidth={true}
-                className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-              >
-                Add to Cart
-              </Button>
+              <a href="https://www.hotelesestelar.com/es/hotel/intercontinental-cali">
+                <Button
+                  ripple={false}
+                  fullWidth={true}
+                  className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
+
+                >
+                  Página principal
+                </Button>
+              </a>
             </CardFooter>
           </Card>
         </Grid>
 
 
         <Grid item xs={12} sm={6} md={3}>
-          <Card className="w-96">
+          <Card className="w-66">
             <CardHeader shadow={false} floated={false} className="h-96">
               <img
-                src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+                src="https://cf.bstatic.com/xdata/images/hotel/square200/240980580.jpg?k=484ee8ce4ef893245ce9c3c17f0f614ee03c6647810f6917c07cce66a14cc018&o="
                 className="w-full h-full object-cover"
               />
             </CardHeader>
             <CardBody>
               <div className="flex items-center justify-between mb-2">
                 <Typography color="blue-gray" className="font-medium">
-                  Apple AirPods
+                  Alko Hotel Casa Nispero
                 </Typography>
                 <Typography color="blue-gray" className="font-medium">
-                  $95.00
+                  $612.000
                 </Typography>
               </div>
               <Typography variant="small" color="gray" className="font-normal opacity-75">
-                With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.
-              </Typography>
+                Habitación Doble              </Typography>
             </CardBody>
             <CardFooter className="pt-0">
-              <Button
-                ripple={false}
-                fullWidth={true}
-                className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-              >
-                Add to Cart
-              </Button>
-            </CardFooter>
-          </Card>
-        </Grid>
+              <a href="https://www.alkohoteles.com/es/hotel-alko-hotel-casa-nispero-en-cali/">
+                <Button
+                  ripple={false}
+                  fullWidth={true}
+                  className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
 
-        <Grid item xs={12} sm={6} md={3}>
-          <Card className="w-96">
-            <CardHeader shadow={false} floated={false} className="h-96">
-              <img
-                src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
-                className="w-full h-full object-cover"
-              />
-            </CardHeader>
-            <CardBody>
-              <div className="flex items-center justify-between mb-2">
-                <Typography color="blue-gray" className="font-medium">
-                  Apple AirPods
-                </Typography>
-                <Typography color="blue-gray" className="font-medium">
-                  $95.00
-                </Typography>
-              </div>
-              <Typography variant="small" color="gray" className="font-normal opacity-75">
-                With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.
-              </Typography>
-            </CardBody>
-            <CardFooter className="pt-0">
-              <Button
-                ripple={false}
-                fullWidth={true}
-                className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-              >
-                Add to Cart
-              </Button>
+                >
+                  Página principal
+                </Button>
+              </a>
             </CardFooter>
           </Card>
         </Grid>
